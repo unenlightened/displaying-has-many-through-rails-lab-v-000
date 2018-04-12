1f77498456a08e87ae916ca917015b9f0b36ce73
Chely Ho <adept_inept@yahoo.com>
2018-04-12 01:19:18 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
+
+  def date
+    self.appointment_datetime.strftime("%m/%d/%Y")
+  end
+
+  def time
+    self.appointment_datetime.strftime("%m/%d/%Y")
+  end
 end
